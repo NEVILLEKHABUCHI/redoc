395486d704189090e51cbfd417eb0e43e36679b9
Neville Khabuchi <nevillekhabuchi@gmail.com>
2024-09-29 11:07:26 +0300

    New Change

--- src/components/Account.js
+++ src/components/Account.js
@@ -1,4 +1,7 @@
+import React, {useState} from 'react';
+
 const Account = () => {
+    const [currentPage, setCurrentPage] = useState('peer-review');
     return (
         <div className="account" >
             <header className="acc-header">
@@ -12,17 +15,35 @@ const Account = () => {
                 <div className="left">
                     <h4>JIMMY RUBIA</h4>
                     <nav id="nav-bar">
-                        <div>
+                        <a href=''>
                             <p>Add Note</p>
                             <i class="fa-solid fa-plus"></i>
-                        </div>
-                        <div>Peer Review</div>
-                        <div>Socials</div>
-                        <div>Chats</div>
-                        <div>My Works</div>
+                        </a>
+                        <a href='#' onClick={()=>setCurrentPage('peer-review')}>Peer Review</a>
+                        <a href='#' onClick={()=>setCurrentPage('socials')}>Socials</a>
+                        <a href='#' onClick={()=>setCurrentPage('chats')}>Chats</a>
+                        <a href='#'>My Works</a>
                     </nav>
                 </div>
-                <div className="right"></div>
+                <div className="right">
+                   {currentPage==='peer-review' && (
+                     <div className='nav-content' id='peer-review'>
+                     This is the peer review page
+                 </div>
+                   )}
+
+                   {currentPage==='socials' && (
+                     <div className='nav-content' id='socials'>
+                     This is the socials page
+                 </div>
+                   )}
+
+                    {currentPage==='chats' && (
+                        <div className='nav-content' id='chats'>
+                        This is the chats page
+                    </div>
+                    )}
+                </div>
             </div>
         </div>
     )
